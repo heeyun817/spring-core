ddee0a4ec7cde41a7aedc519109313bcff84977c
heeyun817 <vmffpfm79@naver.com>
2024-01-25 17:59:46 +0900

    스프링으로 전환하기
    AppConfig 스프링 기반으로 변경하고 MemberAppOrderApp에 스프링 컨테이너 적용함

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,28 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { //애플리케이션 전체를 설정하고 구성한다
 
-  // 생성자 주입
+  @Bean //스프링 컨테이너에 등록된다
   public MemberService memberService(){
     return new MemberServiceImpl(memberRepository()); //ctrl + art + m
   }
 
-  private MemberRepository memberRepository() {
+  @Bean
+  public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService(){
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
+  @Bean
   public DiscountPolicy discountPolicy(){
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 // 순수 자바로 테스트 작성
 public class MemberApp {
 
   public static void main(String[] args) { //psvm
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // AppConfig에 있는 환경 설정 정보를 스프링 컨테이너에 넣고 다 관리함
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//memberService메서드 이름을 찾음(타입은 MemberService인거)
+
     Member member = new Member(1L, "memberA", Grade.VIP); //ctrl + art + v
     memberService.join(member);
 
