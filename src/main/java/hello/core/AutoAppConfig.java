d395a7a4a3053748afccbaa9f2e98a95b0d2acbf
heeyun817 <vmffpfm79@naver.com>
2024-02-01 13:46:21 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( //@어노테이션이 붙은 클래스를 찾아서 자동으로 스프링 빈으로 등록해줌
+//    basePackages = "hello.core.member",
+//    basePackageClasses = AutoAppConfig.class,
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //뺄 어노테이션을 지정 -> 기존 예제 코드를 제외하기 위해
 )
 public class AutoAppConfig {
