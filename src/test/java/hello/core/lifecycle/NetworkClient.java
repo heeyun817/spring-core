346cabf166541bdb4f391180bad413fe07b2ee8f
heeyun817 <vmffpfm79@naver.com>
2024-02-05 14:35:08 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
   private String url;
@@ -26,12 +29,14 @@ public class NetworkClient {
     System.out.println("close " + url);
   }
 
+  @PostConstruct
   public void init() { //의존 관계 주입 다 끝나면 호출
     System.out.println("NetworkClient.init");
     connect();
     call("초기화 연결 메시지");
   }
 
+  @PreDestroy
   public void close() { //종료될 때 호출
     System.out.println("NetworkClient.close");
     disconnect();
